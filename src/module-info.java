8fc442c152b88cd486f7aac1df4d0ecf172fe1c0
reinaldocantidio <reinaldocantidio@gmail.com>
2023-03-03 12:39:07 -0300

    First commit - Java

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author cantidio
+ *
+ */
+module primeiro_programa_java {
+}
---
+++ src/primeiro_programa_java/PrimeiroPrograma.java
@@ -0,0 +1,28 @@
+package primeiro_programa_java;
+
+import primeiro_programa_java.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Gato gato = new Gato(null, null, null);
+		Livros livros = new Livros();
+		
+		System.out.println(gato);
+		System.out.println(livros);
+		
+		/* int a = 2;
+		int b = 3;
+		System.out.println("Hello World! =" + (a+b)); */
+		
+	}
+
+}
+
+
+class Livros {
+	private String nome;
+	private String npag;
+}
